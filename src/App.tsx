899a64374208ee996be11fdf058aeac7d00ce1cb
Gabriel Henrique S <gabrielbet4@gmail.com>
2021-05-03 17:14:04 -0300

    remocao do craco

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import GlobalStyles from "./styles/global";
 import Routes from "./routes";
-import './App.less'
+import './App.css'
 import Layout from './containers/Layout'
 import { ThemeProvider } from "styled-components";
 import theme from './styles/theme'
