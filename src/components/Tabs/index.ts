5f14e1ab2dfff4540a18493f583d95b9904e5896
Júlio César <jc10ferreira@gmail.com>
2021-05-05 17:16:51 -0300

    início de ferramentas/api

---
+++ src/components/Tabs/index.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { Tabs } from "antd";
+
+const { TabPane } = Tabs;
+
+export const StyledTab = styled(Tabs)``;
+
+export const StyledTabPane = styled(TabPane)``;
--- src/pages/Tools/Api/index.tsx
+++ src/pages/Tools/Api/index.tsx
@@ -1,9 +1,45 @@
 import React from "react";
+import { ButtonPrimary } from "../../../components/Button";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardSubTitle,
+  CardTitle,
+} from "../../../components/Card";
 
+import { IoReload } from "react-icons/io5";
+import { TextWithIcon } from "../../../components/TextWithIcon";
+import { StyledTab, StyledTabPane } from "../../../components/Tabs";
 // import { Container } from './styles';
 
 const Api: React.FC = () => {
-  return <div>oopa api</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <div>
+          <CardTitle>Chaves da API</CardTitle>
+          <CardSubTitle>Veja as chaves de API ou cadastre novas</CardSubTitle>
+        </div>
+        <ButtonPrimary>
+          <TextWithIcon>
+            Novo token &nbsp;
+            <IoReload />
+          </TextWithIcon>
+        </ButtonPrimary>
+      </CardHeader>
+
+      <CardContent>
+        <StyledTab>
+          <StyledTabPane tab="Token de acesso" key="1">
+            api key
+            <br />
+            api password
+          </StyledTabPane>
+        </StyledTab>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default Api;
