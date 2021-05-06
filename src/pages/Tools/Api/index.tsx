ef77cd4acfce134a69a9065812c2997b28209af3
Júlio César <jc10ferreira@gmail.com>
2021-05-06 10:03:02 -0300

    ferramentas > api feito

--- src/pages/Tools/Api/index.tsx
+++ src/pages/Tools/Api/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+
 import { ButtonPrimary } from "../../../components/Button";
 import {
   Card,
@@ -7,13 +8,67 @@ import {
   CardSubTitle,
   CardTitle,
 } from "../../../components/Card";
-
-import { IoReload } from "react-icons/io5";
 import { TextWithIcon } from "../../../components/TextWithIcon";
 import { StyledTab, StyledTabPane } from "../../../components/Tabs";
+
+import { Typography } from "antd";
+
+import { IoReload } from "react-icons/io5";
+import { FaFileAlt } from "react-icons/fa";
+
 // import { Container } from './styles';
+const { Text } = Typography;
 
 const Api: React.FC = () => {
+  const [isToken, setIsToken] = useState(false);
+
+  const noTokenYetMessage = () => {
+    setIsToken(!isToken);
+    if (isToken) {
+      return (
+        <>
+          <div>
+            <p>API Key</p>
+            <Text copyable>pxpay_534354_agfadkjfgdf</Text>
+          </div>
+
+          <div>
+            <p>API Password</p>
+            <Text copyable>pxpay_534354_agfadkjfgdf</Text>
+          </div>
+
+          <Text>
+            <FaFileAlt />A documentação da API está disponível em:{" "}
+            <a href="http://pudim.com.br">www.link.com.br</a>
+          </Text>
+        </>
+      );
+    } else {
+      return (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <p>
+            <b>Por enquanto, não há tokens de acesso :(</b>
+          </p>
+          <p>Você pode gerar um novo token clicando aqui</p>
+
+          <ButtonPrimary>
+            <TextWithIcon>
+              Novo token &nbsp;
+              <IoReload />
+            </TextWithIcon>
+          </ButtonPrimary>
+        </div>
+      );
+    }
+  };
+
   return (
     <Card>
       <CardHeader>
@@ -32,9 +87,7 @@ const Api: React.FC = () => {
       <CardContent>
         <StyledTab>
           <StyledTabPane tab="Token de acesso" key="1">
-            api key
-            <br />
-            api password
+            {noTokenYetMessage()}
           </StyledTabPane>
         </StyledTab>
       </CardContent>
