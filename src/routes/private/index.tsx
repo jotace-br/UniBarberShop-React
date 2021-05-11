a476073a7514559b32cb274e8e4d226e038574eb
Gabriel Henrique S <gabrielbet4@gmail.com>
2021-05-11 11:21:23 -0300

    ajustes rotas

--- src/routes/private/index.tsx
+++ src/routes/private/index.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 import Layout from "../../containers/Layout";
-
 import { isAuthenticated } from "../../services/login";
 
 const PrivateRoute = ({ component, title, ...rest }: any) => {
   const routeComponent = (props: any) =>
-    isAuthenticated()
-      ? React.createElement(component, props, title)
-      :  <Redirect to={{ pathname: "/login" }} />
-        // React.createElement(component, props, title);
-  return (
+    isAuthenticated() ? (
+      React.createElement(component, props, title)
+    ) : (
+      <Redirect to={{ pathname: "/login" }} />
+    );
+  // React.createElement(component, props, title);
+  return isAuthenticated() ? (
     <Layout>
       <Route {...rest} render={routeComponent} />
     </Layout>
+  ) : (
+    <Redirect to={{ pathname: "/login" }} />
   );
 };
 
